84d5cb70857765091ebbb66088650d74e157dea2
AlexGaiser <alexcgaiser@gmail.com>
2022-03-01 13:45:05 -0500

    move tests and add tests for constants

---
+++ converters/tests/test_constants.py
@@ -0,0 +1,9 @@
+from converters import constants
+
+
+def test_constants():
+    assert constants.SECONDS_CONV_FACTOR == 2.33
+    assert constants.HUMAN_ERIDIAN_DIGITS == {'0': 'ℓ', '1': 'I',
+                                              '2': 'V', '3': 'λ', '4': '+', '5': '⍱'}
+    assert constants.ERIDIAN_HUMAN_DIGITS == {'ℓ': '0', 'I': '1',
+                                              'V': '2', 'λ': '3', '+': '4', '⍱': '5'}
