c4e5c61ddce1bcba1ed945f7c13d8415003ef75d
AlexGaiser <alexcgaiser@gmail.com>
2022-03-05 21:21:23 -0500

    add human atmospheres and seconds to urls

--- app/human/urls.py
+++ app/human/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+h_controller = views.HumanController
+
 urlpatterns = [
-    path('numbers/<int:value>', views.HumanController.getNumber, name='numbers')
+    path('numbers/<int:value>', h_controller.getNumber, name='human_numbers'),
+    path('atmospheres/<int:value>',
+         h_controller.getAtmospheres, name='human_atmos'),
+    path('seconds/<int:value>', h_controller.getSeconds, name='human_seconds')
 ]
