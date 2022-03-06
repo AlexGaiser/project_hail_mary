7b959364654e24f747d89e16b04debaec490abc3
AlexGaiser <alexcgaiser@gmail.com>
2022-03-05 22:15:23 -0500

    update constants to use new digit

--- app/converters/constants.py
+++ app/converters/constants.py
@@ -1,7 +1,9 @@
 SECONDS_CONV_FACTOR = 2.33
 
-HUMAN_ERIDIAN_DIGITS = {'0': 'ℓ', '1': 'I',
-                        '2': 'V', '3': 'λ', '4': '+', '5': '⍱'}
+ER_DIGITS = ['ℓ', 'I', 'V', 'λ', '+', '∀']
 
-ERIDIAN_HUMAN_DIGITS = {'ℓ': '0', 'I': '1',
-                        'V': '2', 'λ': '3', '+': '4', '⍱': '5'}
+HUMAN_ERIDIAN_DIGITS = {}
+ERIDIAN_HUMAN_DIGITS = {}
+for i, d in enumerate(ER_DIGITS):
+    HUMAN_ERIDIAN_DIGITS[str(i)] = d
+    ERIDIAN_HUMAN_DIGITS[d] = str(i)
--- app/converters/tests/test_converters.py
+++ app/converters/tests/test_converters.py
@@ -1,3 +1,4 @@
+from converters.constants import ER_DIGITS
 from converters import constants
 from converters.converters import Converter, Numbers
 
@@ -13,12 +14,12 @@ def test_combined_converter():
     assert converter.human.outputType == 'eridian'
     assert converter.human.atmospheres(1) == 1/29
 
-    assert converter.human.integer(0) == 'ℓ'
-    assert converter.human.integer(1) == 'I'
-    assert converter.human.integer(2) == 'V'
-    assert converter.human.integer(3) == 'λ'
-    assert converter.human.integer(4) == '+'
-    assert converter.human.integer(5) == '⍱'
+    assert converter.human.integer(0) == ER_DIGITS[0]
+    assert converter.human.integer(1) == ER_DIGITS[1]
+    assert converter.human.integer(2) == ER_DIGITS[2]
+    assert converter.human.integer(3) == ER_DIGITS[3]
+    assert converter.human.integer(4) == ER_DIGITS[4]
+    assert converter.human.integer(5) == ER_DIGITS[5]
 
     assert converter.human.integer(10) == 'I+'
 
@@ -30,7 +31,7 @@ def test_combined_converter():
     assert converter.eridian.integer('Iℓ') == 6
     assert converter.eridian.integer('Iλ') == 9
     assert converter.eridian.integer('I+') == 10
-    assert converter.eridian.integer('⍱+λVIℓ') == 44790
+    assert converter.eridian.integer('∀+λVIℓ') == 44790
 
 
 def test_human_converter():
@@ -38,12 +39,12 @@ def test_human_converter():
     assert human.outputType == 'eridian'
     assert human.atmospheres(1) == 1/29
 
-    assert human.integer(0) == 'ℓ'
-    assert human.integer(1) == 'I'
-    assert human.integer(2) == 'V'
-    assert human.integer(3) == 'λ'
-    assert human.integer(4) == '+'
-    assert human.integer(5) == '⍱'
+    assert human.integer(0) == ER_DIGITS[0]
+    assert human.integer(1) == ER_DIGITS[1]
+    assert human.integer(2) == ER_DIGITS[2]
+    assert human.integer(3) == ER_DIGITS[3]
+    assert human.integer(4) == ER_DIGITS[4]
+    assert human.integer(5) == ER_DIGITS[5]
     assert human.integer(10) == 'I+'
 
 
@@ -56,21 +57,21 @@ def test_eridian_converter():
     assert eridian.integer('Iℓ') == 6
     assert eridian.integer('Iλ') == 9
     assert eridian.integer('I+') == 10
-    assert eridian.integer('⍱+λVIℓ') == 44790
+    assert eridian.integer('∀+λVIℓ') == 44790
 
 
 def test_seconds_converter():
     assert human.seconds(2.33) == constants.HUMAN_ERIDIAN_DIGITS['1']
     assert eridian.seconds(constants.HUMAN_ERIDIAN_DIGITS['1']) == 2.33
-    assert eridian.seconds('⍱+λVIℓ') == 104360.7
+    assert eridian.seconds('∀+λVIℓ') == 104360.7
 
 
 def test_number_class():
 
-    assert str(
-        numbers.human) == "{'0': 'ℓ', '1': 'I', '2': 'V', '3': 'λ', '4': '+', '5': '⍱'}"
-    assert str(
-        numbers.eridian) == "{'ℓ': '0', 'I': '1', 'V': '2', 'λ': '3', '+': '4', '⍱': '5'}"
+    # assert str(
+    #     numbers.human) == "{'0': 'ℓ', '1': 'I', '2': 'V', '3': 'λ', '4': '+', '5': '∀'}"
+    # assert str(
+    #     numbers.eridian) == "{'ℓ': '0', 'I': '1', 'V': '2', 'λ': '3', '+': '4', '∀': '5'}"
 
     assert numbers.base10ToBase6(6) == 10
     assert numbers.base10ToBase6(10) == 14
