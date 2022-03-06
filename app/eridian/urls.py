10a6e8a4c2e0858057971896fe46a293fe25f087
AlexGaiser <alexcgaiser@gmail.com>
2022-03-05 23:18:28 -0500

    convert numbers to number in path

--- app/eridian/urls.py
+++ app/eridian/urls.py
@@ -4,7 +4,7 @@ from . import views
 e_controller = views.EridianController
 
 urlpatterns = [
-    path('numbers/<str:value>', e_controller.getNumber, name='eridian_numbers'),
+    path('number/<str:value>', e_controller.getNumber, name='eridian_numbers'),
     path('atmospheres/<int:value>',
          e_controller.getAtmospheres, name='eridian_atmos'),
     path('seconds/<str:value>', e_controller.getSeconds, name='eridian_seconds'),
