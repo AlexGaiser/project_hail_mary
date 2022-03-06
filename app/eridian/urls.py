96d9bcfe205535c6d5ed31ebecb17f0a16a89549
AlexGaiser <alexcgaiser@gmail.com>
2022-03-05 21:16:44 -0500

    add seconds and atmospheres urls to eridian app

--- app/eridian/urls.py
+++ app/eridian/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
 
+e_controller = views.EridianController
+
 urlpatterns = [
-    path('numbers/<str:value>', views.ConverterController.getNumber, name='eridian')
+    path('numbers/<str:value>', e_controller.getNumber, name='eridian_numbers'),
+    path('atmospheres/<int:value>',
+         e_controller.getAtmospheres, name='eridian_atmos'),
+    path('seconds/<str:value>', e_controller.getSeconds, name='eridian_seconds'),
 ]
