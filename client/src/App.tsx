ebf9010ac27ee69505283a301014dd1408792ee6
AlexGaiser <alexcgaiser@gmail.com>
2022-03-06 22:10:53 -0500

    remove extra components from main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,17 @@
 import React, { useEffect } from 'react';
-import './App.css';
-import { EridianClock } from './components/EridianClock';
-import { fetchHumanNumberToEridian } from './services/converters.service';
-import { Input } from './shared/Input';
+// import './App.css';
+import { fetchHumanNumberToEridian } from './services/api/conversion.service';
 import SecondsConverter from './views/Converters/SecondsConverter';
 
 function App() {
-  useEffect(() => {
-    fetchHumanNumberToEridian(123).then((data) => console.log(data));
-  });
-
   return (
-    <div className="App">
+    <div className="App p-10">
       <h1 className="text-3xl font-bold underline">
         Human Eridian Conversion Tool
       </h1>
-      <SecondsConverter />
-
-      <h2>Eridian Clock</h2>
-      <EridianClock />
-      <h2>Converters</h2>
-
-      <Input
-        label="numbers"
-        value="1234"
-        onChange={() => console.log('hello')}
-      />
+      <div className="w-1/2 p-10">
+        <SecondsConverter />
+      </div>
     </div>
   );
 }
