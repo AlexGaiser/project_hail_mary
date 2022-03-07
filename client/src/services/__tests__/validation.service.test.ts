5cc81ae3c6dccc54fa18aa1a8d477ba234711d19
AlexGaiser <alexcgaiser@gmail.com>
2022-03-06 20:17:30 -0500

    improve validation for human numbers

--- client/src/services/__tests__/validation.service.test.ts
+++ client/src/services/__tests__/validation.service.test.ts
@@ -1,4 +1,7 @@
-import { isValidEridianString } from '../validation.service';
+import {
+  isValidEridianString,
+  isValidHumanNumber,
+} from '../validation.service';
 
 describe('testing eridian validation service', () => {
   test('should validate string against eridian digits', () => {
@@ -6,5 +9,19 @@ describe('testing eridian validation service', () => {
     expect(isValidEridianString('VVVℓℓ+∀+λ')).toBe(true);
     expect(isValidEridianString('VVVℓℓ+∀+λ1')).toBe(false);
     expect(isValidEridianString('')).toBe(true);
+  });
+});
+
+describe('validation of human number input', () => {
+  test('should test if string or numbers are valid', () => {
+    expect(isValidHumanNumber('123')).toBe(true);
+    expect(isValidHumanNumber('abc123')).toBe(false);
+    expect(isValidHumanNumber(12345)).toBe(true);
+    expect(isValidHumanNumber('')).toBe(true);
+    expect(isValidHumanNumber(-12345)).toBe(true);
+    expect(isValidHumanNumber(123.45)).toBe(true);
+    expect(isValidHumanNumber('-123.45')).toBe(true);
+    expect(isValidHumanNumber('12345.3d')).toBe(false);
+    expect(isValidHumanNumber('1e10000')).toBe(false);
   });
 });
--- client/src/services/validation.service.ts
+++ client/src/services/validation.service.ts
@@ -1,10 +1,17 @@
 import { ER_DIGITS_DICT } from '../constants';
 
+const numberRegex = /^-?\d*\.?\d*$/;
+
 export const isValidEridianString = (str: string): boolean => {
   for (const c of str) {
     if (!ER_DIGITS_DICT[c] && ER_DIGITS_DICT[c] !== 0) {
       return false;
     }
   }
+  return true;
+};
+
+export const isValidHumanNumber = (num: number | string) => {
+  if (typeof num === 'string') return numberRegex.test(num); // ...and ensure strings of whitespace fail
   return true;
 };
