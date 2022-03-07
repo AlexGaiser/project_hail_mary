eab2e7c57a8054fe1fed4cbd2947bc3bb773c5da
AlexGaiser <alexcgaiser@gmail.com>
2022-03-06 22:19:16 -0500

    Add Eridian Keyboard with delete Key

--- client/src/components/EridianKeyboard/EridianKeyboard.tsx
+++ client/src/components/EridianKeyboard/EridianKeyboard.tsx
@@ -4,21 +4,35 @@ import { Keyboard } from '../../shared/Keyboard/Keyboard';
 
 interface IProps {
   onClick: (digit: string) => void;
+  clearFunction?: () => void;
 }
 
-export const EridianKeyboard = ({ onClick }: IProps) => {
-  const keys = ER_DIGITS.map((d) => (
-    <button
-      className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={() => onClick(d)}
-    >
+const styles = {
+  buttonClass: 'btn btn-green',
+};
+
+export const EridianKeyboard = ({
+  onClick,
+  clearFunction,
+}: IProps) => {
+  const symbolKeys = ER_DIGITS.map((d) => (
+    <button className={styles.buttonClass} onClick={() => onClick(d)}>
       {d}
     </button>
   ));
 
+  const deleteKey =
+    clearFunction != undefined ? (
+      <button className={styles.buttonClass} onClick={clearFunction}>
+        Clear
+      </button>
+    ) : undefined;
+
   return (
     <div>
-      <Keyboard keys={keys} />
+      <Keyboard
+        keys={deleteKey ? [...symbolKeys, deleteKey] : symbolKeys}
+      />
     </div>
   );
 };
