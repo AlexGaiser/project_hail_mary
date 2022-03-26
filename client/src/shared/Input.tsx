4af324427d52f64ab73e1a9a9b166978e88d1b29
AlexGaiser <alexcgaiser@gmail.com>
2022-03-26 17:31:11 -0400

    add onchange handler for input.tsx

--- client/src/shared/Input.tsx
+++ client/src/shared/Input.tsx
@@ -10,9 +10,15 @@ export const Input = (props: IProps) => {
   const { label, value, onChange } = props;
 
   return (
-    <div>
-      <label>{label}</label>
-      <input onChange={onChange} value={value} />
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        {label}
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 };
